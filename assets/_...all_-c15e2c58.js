2c888e05e0ab1e9e68649c086fb71abeab01e8bb
rxlxr11 <rxlxr11@users.noreply.github.com>
2024-07-11 14:14:09 +0000

    deploy: 87d024cbe4e461052e4115e536af4eb079877c85

---
+++ assets/_...all_-c15e2c58.js
@@ -0,0 +1,1 @@
+import{g as a,ak as s,u as n,am as r,o as c,i as p}from"./app-e4bd3734.js";import{b as t}from"./route-block-83d24a4e.js";const u=a({__name:"[...all]",setup(i){const o=s(),e=n();return r(()=>{o.push("/"),e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(f,l)=>(c(),p("div"))}});typeof t=="function"&&t(u);export{u as default};
